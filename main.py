0914c23d38b357982cfe39db0b5d7e35cbe7d5fd
cnold <110209845+nold4@users.noreply.github.com>
2024-11-11 16:52:39 -0500

    set game to run at 60fps

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from constants import *
 
 
 def main():
+    # basic pygame setup
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     color = 0,0,0
+
+    # pygame clock object to keep track of time elapsed - dt is delta time (aka time passed since last update)
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +19,8 @@ def main():
             
         screen.fill(color)
         pygame.display.flip()
+        # limit the framerate to 60fps
+        dt = clock.tick(60) / 1000
 
 
 
