519bf6225981e30eb2ba339deac2e5f7a75de46d
cnold <110209845+nold4@users.noreply.github.com>
2024-11-12 17:24:44 -0500

    added player object to pygame group for better control of game objects to draw and update

--- main.py
+++ main.py
@@ -8,27 +8,38 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     color = 0,0,0
+
+    # creating groups
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # pygame clock object to keep track of time elapsed - dt is delta time (aka time passed since last update)
     clock = pygame.time.Clock()
     dt = 0
 
-    # creating player object(s)
-    
+  
+
+    updateable.add(player)
+    drawable.add(player)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
-            
+        for obj in updateable:
+            obj.update(dt)
+
         screen.fill(color)
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
+        
         pygame.display.flip()
-
-
         # limit the framerate to 60fps
         dt = clock.tick(60) / 1000
 
