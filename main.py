4ef5a1f960d37fc181757ae5bf1b9ffa8fb1ff64
cnold <110209845+nold4@users.noreply.github.com>
2024-11-15 00:12:00 -0500

    added shooting from player object (no collision with asteroids yet)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
@@ -16,9 +17,12 @@ def main():
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
+    
     Player.containers = (updateable, drawable)
     Asteroid.containers = (asteroids, updateable, drawable)
+    Shot.containers = (shots, updateable, drawable)
     AsteroidField.containers = (updateable)
 
     asteroid_field = AsteroidField()
@@ -40,7 +44,7 @@ def main():
             
         for obj in updateable:
             obj.update(dt)
-
+        
 
         for asteroid in asteroids:
             if asteroid.collides_with(player):
@@ -51,8 +55,7 @@ def main():
         
         for obj in drawable:
             obj.draw(screen)
-
-        
+ 
         pygame.display.flip()
         # limit the framerate to 60fps
         dt = clock.tick(60) / 1000
