1bfdc8439224bd69a6c37bca5504d269e811b538
cnold <110209845+nold4@users.noreply.github.com>
2024-11-14 23:46:42 -0500

    added collision detection between player and asteroids - created new empty class for shooting

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from constants import *
 from player import Player
@@ -40,6 +41,12 @@ def main():
         for obj in updateable:
             obj.update(dt)
 
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+                
         screen.fill(color)
         
         for obj in drawable:
